5ff5c6bd504263e56e6e9d438ed9ce7c0885b82c
guilherme daniel <gui.dani13@gmail.com>
2023-02-14 17:25:06 -0300

    adicionado middleware para proteger páginas

---
+++ src/pages/dashboard/index.tsx
@@ -0,0 +1,9 @@
+export default function Dashboard() {
+  return (
+    <>
+      <h1>Dashboard</h1>
+      <p>Está página deve ser acessada apenas por usuários logados.</p>
+      <div>Dashboard</div>
+    </>
+  );
+}
